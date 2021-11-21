35215c83a5405615e9d3dbc50e86a1661aa8658a
Rolf <rolf@yoast.com>
2021-11-21 14:00:31 +0100

    Y2015 D01 solution

--- src/Controller/Y2015/D01.php
+++ src/Controller/Y2015/D01.php
@@ -7,8 +7,8 @@ use Symfony\Component\HttpFoundation\Response;
 class D01 extends AbstractController {
 	public function puzzle( $puzzle ): Response {
 
-		$answer1 = 'Empty solution 1';
-		$answer2 = 'Empty solution 2';
+		$answer1 = $this->puzzle1( $puzzle );
+		$answer2 = $this->puzzle2( $puzzle );
 
 		return $this->json(
 			array(
@@ -16,5 +16,35 @@ class D01 extends AbstractController {
 				'answer2' => $answer2,
 			)
 		);
+	}
+
+	private function puzzle1( $puzzle ): string {
+		$up   = substr_count( $puzzle, '(' );
+		$down = substr_count( $puzzle, ')' );
+		return intval( $up ) - intval( $down );
+	}
+
+	private function puzzle2( $puzzle ): string {
+		$puzzle       = str_replace( '(', '(,', $puzzle );
+		$puzzle       = str_replace( ')', '),', $puzzle );
+		$puzzle_array = explode( ',', $puzzle );
+		$floor        = 0;
+		$count        = 1;
+		foreach ( $puzzle_array as $something ) {
+			if ( $something === '(' ) {
+				$floor++;
+			}
+
+			if ( $something === ')' ) {
+				$floor--;
+			}
+
+			if ( $floor < 0 ) {
+
+				break;
+			}
+			$count++;
+		}
+		return $count;
 	}
 }
