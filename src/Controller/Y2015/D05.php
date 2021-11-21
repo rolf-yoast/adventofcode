6a3605ed83e528ad1afded2c8ea1e0e57461eca9
Rolf <rolf@yoast.com>
2021-11-21 19:28:46 +0100

    Y2015 D05 half

--- src/Controller/Y2015/D05.php
+++ src/Controller/Y2015/D05.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 class D05 extends AbstractController {
 	public function puzzle( $puzzle ): Response {
 
-		$answer1 = 'Empty solution 1';
+		$answer1 = $this->puzzle1( $puzzle );
 		$answer2 = 'Empty solution 2';
 
 		return $this->json(
@@ -16,5 +16,53 @@ class D05 extends AbstractController {
 				'answer2' => $answer2,
 			)
 		);
+	}
+
+	private function puzzle1( $puzzle ): string {
+		$lines = explode( "\r\n", trim( $puzzle ) );
+
+		$vowels    = array( 'a', 'e', 'i', 'o', 'u' );
+		$bad_combo = array( 'ab', 'cd', 'pq', 'xy' );
+
+		$total = 0;
+
+		foreach ( $lines as $line ) {
+			$letters     = str_split( $line );
+			$lettercount = count( $letters );
+
+			$count = preg_match_all( '/[aeiou]/i', $line, $matches );
+
+			$double = 0;
+			if ( $count > 2 ) {
+
+				$i = 1;
+				foreach ( $letters as $letter ) {
+					if ( $lettercount === $i ) {
+						break;
+					}
+
+					if ( $letter === $letters[ $i ] ) {
+						$double = 1;
+						break;
+					}
+					$i++;
+				}
+			}
+
+			$bad = 0;
+			if ( $double === 1 ) {
+				foreach ( $bad_combo as $bad_combi ) {
+					if ( str_contains( $line, $bad_combi ) ) {
+						$bad = 1;
+					}
+				}
+			}
+
+			if ( $bad === 0 && $double === 1 && $count > 2 ) {
+				$total++;
+			}
+		}
+
+		return $total;
 	}
 }
