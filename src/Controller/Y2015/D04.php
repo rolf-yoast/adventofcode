49a001b03853f08ab857183873cd28eda85df75d
Rolf <rolf@yoast.com>
2021-11-21 17:32:53 +0100

    Y2015 D04 solution

--- src/Controller/Y2015/D04.php
+++ src/Controller/Y2015/D04.php
@@ -1,14 +1,16 @@
 <?php
 namespace App\Controller\Y2015;
 
+ini_set( 'max_execution_time', '0' );
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class D04 extends AbstractController {
 	public function puzzle( $puzzle ): Response {
 
-		$answer1 = 'Empty solution 1';
-		$answer2 = 'Empty solution 2';
+		$answer1 = $this->puzzle1( $puzzle );
+		$answer2 = $this->puzzle2( $puzzle );
 
 		return $this->json(
 			array(
@@ -16,5 +18,39 @@ class D04 extends AbstractController {
 				'answer2' => $answer2,
 			)
 		);
+	}
+
+	private function puzzle1( $puzzle ) {
+
+		$i = 0;
+		while ( ++$i ) {
+
+			$hash = md5( $puzzle . $i );
+
+			$result = substr( $hash, 0, 5 );
+
+			if ( $result === '00000' ) {
+				break;
+			}
+		}
+
+		return $i;
+	}
+
+	private function puzzle2( $puzzle ) {
+
+		$i = 0;
+		while ( ++$i ) {
+
+			$hash = md5( $puzzle . $i );
+
+			$result = substr( $hash, 0, 6 );
+
+			if ( $result === '000000' ) {
+				break;
+			}
+		}
+
+		return $i;
 	}
 }
