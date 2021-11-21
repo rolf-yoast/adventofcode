babd76293e7cc707a993045db9e7b0078435723c
Rolf <rolf@yoast.com>
2021-11-21 14:59:56 +0100

    Y2015 D03 solution

--- src/Controller/Y2015/D03.php
+++ src/Controller/Y2015/D03.php
@@ -7,8 +7,8 @@ use Symfony\Component\HttpFoundation\Response;
 class D03 extends AbstractController {
 	public function puzzle( $puzzle ): Response {
 
-		$answer1 = 'Empty solution 1';
-		$answer2 = 'Empty solution 2';
+		$answer1 = $this->puzzle1( $puzzle );
+		$answer2 = $this->puzzle2( $puzzle );
 
 		return $this->json(
 			array(
@@ -16,5 +16,111 @@ class D03 extends AbstractController {
 				'answer2' => $answer2,
 			)
 		);
+	}
+
+	private function puzzle1( $puzzle ) {
+		$puzzle = str_replace( '^', '^,', $puzzle );
+		$puzzle = str_replace( 'v', 'v,', $puzzle );
+		$puzzle = str_replace( '>', '>,', $puzzle );
+		$puzzle = str_replace( '<', '<,', $puzzle );
+		$puzzle = substr( $puzzle, 0, -1 );
+		$moves  = explode( ',', $puzzle );
+
+		$horizontal  = 0;
+		$vertical    = 0;
+		$coordinates = array( $horizontal . ',' . $vertical );
+		foreach ( $moves as $move ) {
+			if ( $move === '^' ) {
+				$horizontal++;
+			}
+
+			if ( $move === 'v' ) {
+				$horizontal--;
+			}
+
+			if ( $move === '>' ) {
+				$vertical++;
+			}
+
+			if ( $move === '<' ) {
+				$vertical--;
+			}
+
+			$coordinates[] = $horizontal . ',' . $vertical;
+		}
+
+		$coordinates = array_unique( $coordinates );
+
+		return count( $coordinates );
+	}
+
+	private function puzzle2( $puzzle ) {
+		$puzzle = str_replace( '^', '^,', $puzzle );
+		$puzzle = str_replace( 'v', 'v,', $puzzle );
+		$puzzle = str_replace( '>', '>,', $puzzle );
+		$puzzle = str_replace( '<', '<,', $puzzle );
+		$puzzle = substr( $puzzle, 0, -1 );
+		$moves  = explode( ',', $puzzle );
+
+		$horizontal_santa = 0;
+		$vertical_santa   = 0;
+
+		$horizontal_robot = 0;
+		$vertical_robot   = 0;
+
+		$coordinates_santa = array( $horizontal_santa . ',' . $vertical_santa );
+		$coordinates_robot = array( $horizontal_robot . ',' . $vertical_robot );
+
+		$i = 0;
+		foreach ( $moves as $move ) {
+			if ( $i % 2 === 0 ) {
+				if ( $move === '^' ) {
+					$horizontal_santa++;
+				}
+
+				if ( $move === 'v' ) {
+					$horizontal_santa--;
+				}
+
+				if ( $move === '>' ) {
+					$vertical_santa++;
+				}
+
+				if ( $move === '<' ) {
+					$vertical_santa--;
+				}
+
+				$coordinates_santa[] = $horizontal_santa . ',' . $vertical_santa;
+
+			} else {
+
+				if ( $move === '^' ) {
+					$horizontal_robot++;
+				}
+
+				if ( $move === 'v' ) {
+					$horizontal_robot--;
+				}
+
+				if ( $move === '>' ) {
+					$vertical_robot++;
+				}
+
+				if ( $move === '<' ) {
+					$vertical_robot--;
+				}
+
+				$coordinates_robot[] = $horizontal_robot . ',' . $vertical_robot;
+
+			}
+
+			$i++;
+		}
+
+		$coordinates = array_merge( $coordinates_robot, $coordinates_santa );
+
+		$coordinates = array_unique( $coordinates );
+
+		return count( $coordinates );
 	}
 }
